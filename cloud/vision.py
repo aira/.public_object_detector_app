058483a886f244040f59403744551dc61cb51080
Ashwin Kannan <ashwin@Ashwins-MacBook-Pro.local>
2017-12-19 12:08:52 -0800

    working with numpy arrays now

--- cloud/vision.py
+++ cloud/vision.py
@@ -2,15 +2,24 @@ import io
 import os
 from google.cloud import vision
 from google.cloud.vision import types
+from skimage.data import coffee
+from PIL import Image
 
 # setting the JSON file to be the environment variable required by GCV
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'apikey.json'
+
+# initiate coffee numpy array
+img = coffee()
+
+# convert to an image using PIL and then saving the file
+pil_img = Image.fromarray(img, 'RGB')
+pil_img.save('image.jpg', 'JPEG')
 
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
 # name of image file to annotate
-image_file_name = 'andre.jpg'
+image_file_name = 'image.jpg'
 
 with io.open(image_file_name, 'rb') as image_file:
     content = image_file.read()
